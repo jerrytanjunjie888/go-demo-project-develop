bad301d0ff3d43312a1960c3bfb2309b965e0021
jerrytanjunjie888 <48821510+jerrytanjunjie888@users.noreply.github.com>
2020-01-01 11:04:33 +0800

    <feat:>: update go-demo-project-develop

---
+++ userAuth/config/db.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"fmt"
+
+	model "github.com/obed/demo-project/userAuth/model"
+
+	"github.com/jinzhu/gorm"
+	// Not needed for import.
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// Object used to access database tables and executing queries
+var Database *gorm.DB
+var user model.User
+
+func init() {
+	var err error
+	// TODO: Remove sensitive data tomorrow
+	Database, err = gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=123456 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	// set this to 'true' to see sql logs
+	Database.LogMode(true)
+	Database.AutoMigrate(&user)
+	fmt.Println("Database connection successful.")
+}
+func CloseDB() {
+	defer Database.Close()
+}
